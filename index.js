9139a2d54439d699acf5a0affd2327ab43e66af5
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-10-30 16:28:42 +0000

    Lyndsey

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getImages(query) {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+  );
+  const { data } = await response.json();
+  console.log(data[0].url);
+}
+getImages("cat");
